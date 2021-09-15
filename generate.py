36f5145c777a2e044ebc02c212e257c56f5a8783
Katie McLaughlin <katie@glasnt.com>
2021-09-15 11:29:37 +1000

    cleanup, formatting

--- generate.py
+++ generate.py
@@ -4,17 +4,21 @@
 Take semi-structured yaml data and generate README
 """
 import os
-import requests
+from datetime import datetime
 from pathlib import Path
 from urllib.parse import urlparse
 
+import requests
 import yaml
 from tomark import Tomark
+
+script_start = datetime.now()
+
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", None)
 
 readme = []
 data = []
 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", None)
 
 # Append Header
 with open("_templates/header.md") as f:
@@ -25,21 +29,21 @@ for datafile in sorted(Path("_data").glob("*.yml")):
 
     def valid(s):
         return True if s in info.keys() and info[s] is not None else False
-    
+
     def get_status_code(uri):
         try:
             response = get_url(f"https://{uri}")
-        except requests.exceptions.ConnectTimeout: 
+        except requests.exceptions.ConnectTimeout:
             return "Timeout"
         except requests.exceptions.SSLError:
-            try: 
+            try:
                 response = get_url(f"http://{uri}")
                 if "default web page for this server" in response.text:
                     return "Invalid config"
             except requests.exceptions.SSLError:
                 return "SSL Error"
-         
-        return response.status_code 
+
+        return response.status_code
 
     def get_url(uri, json=False):
         headers = {"Authorization": f"token {GITHUB_TOKEN}"}
@@ -55,30 +59,28 @@ for datafile in sorted(Path("_data").glob("*.yml")):
             "wayback": "",
             "pyvideo": "",
             "repo": "",
-            "repo_url": "",
         }
 
         if valid("canonical_url"):
             url = info["canonical_url"]
-            year["status"] = get_status_code(url)
+            year["status"] = str(get_status_code(url))
             year["url"] = f"[{url}]({url})"
 
         if valid("wayback"):
-            year["wayback"] = f"[Wayback]({info['wayback']})"
+            year["status"] += f" ([Wayback]({info['wayback']}))"
 
         if "repo" in info.keys() and info["repo"] is not None:
-            repo = info['repo']
-            year["repo"] = f"[Repo]({repo})"
+            repo = info["repo"]
+            year["repo"] = f"[{repo}](https://github.com/{repo})"
             response = get_url(f"https://api.github.com/repos/{repo}", json=True)
             if "homepage" in response.keys():
-                year["repo_url"] = response["homepage"]
-
+                year["repo"] += f" ([url](response['homepage']))"
 
         if valid("pyvideo"):
             pyvideo = info["pyvideo"]
             response = get_url(pyvideo)
             talk_count = response.text.count('class="entry-title"')
-            year["pyvideo"] = f"[PyVideo]({info['pyvideo']}) ({talk_count} entries)"
+            year["pyvideo"] = f"[{talk_count} entries]({info['pyvideo']})"
 
         if valid("youtube") and type(info["youtube"]) == list:
             counter = 1
@@ -100,3 +102,5 @@ with open("_templates/footer.md") as f:
 # Write to Disk
 with open("README.md", "w") as f:
     f.write("\n".join(readme))
+
+print(f"Generated README.md in {datetime.now() - script_start}")
